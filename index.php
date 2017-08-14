2e29996c2826d5ce272964093a8b2a742a55f5c3
scr1pter <miguel_cordeiro2017@outlook.pt>
2017-08-14 04:12:03 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+	<body>
+		<form method="post" action="calcular.php" >
+		
+			</fieldset><h1>Calculadora</h1><fieldset/>
+			
+				Primeiro Número:
+				<input type="text" name="num1" size="5" /> <br></p>
+	        
+				<input type="radio" name="oper" value="somar" checked> Somar <br>
+				<input type="radio" name="oper" value="subtrair"> Subtrair <br>
+				<input type="radio" name="oper" value="multiplicar"> Multiplicar <br>
+				<input type="radio" name="oper" value="dividir"> Dividir <br><p>
+	        
+				Segundo Número:
+				<input type="text" name="num2" size="5" /> </br></p>
+
+				<input type="submit"  name="calcular" value="Calcular" />
+			</fieldset>
+			
+	</body>
+<html>
